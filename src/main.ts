7e94ed21b1eac426bd96f8044769fb9519cbc6c0
plastic041 <plastic041@gmail.com>
2022-08-31 09:05:31 +0900

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
 import P5 from "p5";
 
+type CircleConstructorProps = {
+  radius: number;
+  x: number;
+  y: number;
+  color?: number;
+};
+
 class Circle {
   #p5: P5;
   #radius: number;
   #multiplier: number;
+  #pos: P5.Vector;
+  #color: number;
 
-  constructor(p5: P5, { radius = 10 }: { radius: number }) {
+  constructor(p5: P5, { radius, x, y, color = 128 }: CircleConstructorProps) {
     this.#p5 = p5;
     this.#radius = radius;
     this.#multiplier = 1;
+    this.#pos = new P5.Vector(x, y);
+    this.#color = color;
   }
 
   draw() {
@@ -17,9 +28,9 @@ class Circle {
     p5.push();
 
     p5.noStroke();
-    p5.fill(255);
+    p5.fill(this.#color);
 
-    p5.circle(100, 100, this.#radius * this.#multiplier);
+    p5.circle(this.#pos.x, this.#pos.y, this.#radius * this.#multiplier);
 
     p5.pop();
   }
@@ -34,13 +45,23 @@ class Circle {
   }
 }
 
+const WIDTH = 600;
+const HEIGHT = 600;
+
 const sketch = (p5: P5) => {
   let circles: Circle[] = [];
 
   p5.setup = () => {
-    const canvas = p5.createCanvas(200, 200);
-    canvas.parent("app");
-    circles.push(new Circle(p5, { radius: 50 }));
+    const canvas = p5.createCanvas(WIDTH, HEIGHT);
+    canvas.parent("p5");
+    [...new Array(10)].forEach(() => {
+      const x = p5.random(0, WIDTH);
+      const y = p5.random(0, HEIGHT);
+      const radius = p5.random(WIDTH / 10, WIDTH / 4);
+      const color = p5.random(0, 255);
+      const circle = new Circle(p5, { radius, x, y, color });
+      circles.push(circle);
+    });
   };
 
   p5.draw = () => {
